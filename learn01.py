22a9dda3b3dddb75b8da925bd0064701af24f3a5
Rogerdestroy <roge000000mymail@gmail.com>
2022-03-16 17:58:42 +0800

    基本語法

---
+++ learn01.py
@@ -0,0 +1,65 @@
+"""
+> 大於
+< 小於
+>= 大於等於
+<= 小於等於
+== 等於
+!== 不等於
+--------------------------
+true 真的
+false 假的
+and 且
+or 或
+not 非
+-------------------------
+\   續行(放句尾)
+\\   反單斜號
+\'   單引號
+\''  雙引號
+\n   換行
+\r   
+\t
+-------------------------
+if ___ or ____ :
+    _______
+elif ______ :
+    _______
+else:
+    
+for _ in range (n,m) n~m-1
+for [變數名稱] in range(n): (縮排) print([變數名稱])
+
+-------------------------
+"Ro"+"ger"== "Roger"
+-------------------------
+list
+list.append(x): 把變數x塞到list的最後面
+list.insert(i, x): 把變數x塞到i這個位置上
+list.pop(): 把list的最後一格丟掉
+list.pop(i): 把list的第i格丟掉
+list.remove(x): 會把第一個出現的變數x拿掉
+list.clear(): 把list內的資料全部清光光
+max(list): 找出list中最大值
+min(list): 找出list中最小值
+sum(list): 找出list數字總和
+
+slice語法：
+list[start: end]，start和end都可以省略不寫
+start的預設為0
+end的預設為len(list)
+liest[ :end]代表 0~end-1
+list[start: ]代表start~len(list)-1
+list[ : ]代表0~len(list)-1
+
+.lower() 換成小寫
+.upper() 換成大寫
+-------------------------
+
+"""
+
+print('RR\nRRRR\riiiiir'+'ttt'\
+      +'RRRR')
+    
+      
+
+
