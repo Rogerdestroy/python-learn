b9b246e22ac1c77fe7183c68e8537c7a9ec0b708
Rogerdestroy <roge000000mymail@gmail.com>
2022-03-21 21:36:45 +0800

    基本語法

--- learn01.py
+++ learn01.py
@@ -18,7 +18,7 @@ not 非
 \''  雙引號
 \n   換行
 \r   
-\t
+\t   tab 
 -------------------------
 if ___ or ____ :
     _______
@@ -32,13 +32,26 @@ for [變數名稱] in range(n): (縮排) print([變數名稱])
 -------------------------
 "Ro"+"ger"== "Roger"
 -------------------------
-list
+list列表
+
+scores = [0,1,2,3,4,5,6,7]
+frt = ["一","二"]
+print(frt[0])
+print(scores[1:4]) 1~3
+print(scores[1:])  1~end
+print(scores[:4])  0~3
+
+list1.extend(list2)  list1+ist2
 list.append(x): 把變數x塞到list的最後面
 list.insert(i, x): 把變數x塞到i這個位置上
+list.remove(x): 會把第一個出現的變數x拿掉
+list.clear(): 把list內的資料全部清光
 list.pop(): 把list的最後一格丟掉
 list.pop(i): 把list的第i格丟掉
-list.remove(x): 會把第一個出現的變數x拿掉
-list.clear(): 把list內的資料全部清光光
+list.sort()： 從小到大排列
+list.reverse()：列表反轉
+list.index(x)：找第一個x
+list.count(x)：數有幾個x
 max(list): 找出list中最大值
 min(list): 找出list中最小值
 sum(list): 找出list數字總和
@@ -51,15 +64,43 @@ liest[ :end]代表 0~end-1
 list[start: ]代表start~len(list)-1
 list[ : ]代表0~len(list)-1
 
+x="Hello"
+print(x[1:3]) el
 .lower() 換成小寫
 .upper() 換成大寫
+.islower()
+.isupper()
+print(Word[0]) 輸出W
+.index("W") 找W位置 0
+.replace("W","w")  w替換W
 -------------------------
+  
+int('') 
+float(' ')
+str('')
 
+abs(100) 取絕對值
+pow(2,4) 2的4次方
+max(1,2,3,...)
+min(1,2,3,...)
+round(1.5) 4捨5入
+
+from math import *
+floor()無條件捨去
+ceil()無條件進位
+sqrt()開根號
+------------------------
+%d  整數
+%f  浮點數
+%s  字串
+%x  16進制數字
+-----------------------
+ m ,
 """
 
 print('RR\nRRRR\riiiiir'+'ttt'\
       +'RRRR')
-    
+  
       
 
 
